2ac05bf295ba6fa6a145e323312e4977ff510474
ansht2000 <ansht2000@gmail.com>
2024-08-31 16:54:56 -0700

    updated dictionary sorting to be more efficient

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     chars = get_char_freqs(text)
-    dict_list = dict_to_list(chars)
-    dict_list.sort(reverse=True, key=sort_on)
+    sorted_chars = sorted(chars.items(), key=lambda char_freq: char_freq[1], reverse=True)
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{word_count} words found in the document")
-    for dict in dict_list:
-        char = list(dict.keys())[0]
-        if char.isalpha():
-            freq = dict[char]
-            print(f"The '{char}' character was found {freq} times")
+    for char_tuple in sorted_chars:
+        if char_tuple[0].isalpha():
+            print(f"The '{char_tuple[0]}' character was found {char_tuple[1]} times")
     print("--- End Report ---")
 
 
@@ -32,16 +29,5 @@ def get_char_freqs(text):
         else:
             char_freqs[text[i]] += 1
     return char_freqs
-
-def dict_to_list(chars):
-    dict_list = []
-    for k, v in chars.items():
-        small_dict = {}
-        small_dict[k] = v
-        dict_list.append(small_dict)
-    return dict_list
-
-def sort_on(dict):
-    return list(dict.values())[0] 
 
 main()
